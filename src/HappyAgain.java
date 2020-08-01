8c398092b0204f70cfbc75277e01d8700ec3fbea
anastasiiabon <bondar23b@gmail.com>
2020-08-01 12:44:01 -0400

    update HappyAgain.java

--- src/HappyAgain.java
+++ src/HappyAgain.java
@@ -1,5 +1,7 @@
 public class HappyAgain {
     public static void main(String[] args) {
         System.out.println("Super excited and happy");
+        System.out.println("happy");
+        System.out.println("tadaaaam");
     }
 }
